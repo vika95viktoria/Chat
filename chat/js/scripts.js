64fc74c15fe51d93873bd78751ebbefb035f66af
unknown <vika95vika@gmail.com>
2015-04-13 21:14:28 +0300

    Connecting to server

--- chat/js/scripts.js
+++ chat/js/scripts.js
@@ -1,9 +1,10 @@
 /**
  * Created by Администратор on 09.03.2015.
  */
+'use strict';
 
-var Mess;
 var control=0;
+var user="";
 var uniqueId = function() {
     var date = Date.now();
     var random = Math.random() * Math.random();
@@ -14,253 +15,17 @@ var uniqueId = function() {
 
 var theMessage = function(user, message) {
     return {
-        description:user,
-        description2:message,
+        user:user,
+        message:message,
         id: uniqueId()
     };
 };
+var appState = {
+    mainUrl : 'http://localhost:999/chat',
+    messageList:[],
+    token : 'TE11EN'
+};
 
-var MessageList = [];
-
-var labelEl1;
- var user="";
-function whatToDo(){
-    var appContainer = document.getElementsByClassName('Messages')[0];
-    appContainer.addEventListener('click', delegateEvent5);
-}
-
-
-function delegateEvent5(evtObj) {
-
-    if(evtObj.type === 'click' && evtObj.target.classList.contains('btn-warning')){
-         labelEl1 = evtObj.target.parentElement;
-        changeMessage(labelEl1);
-    }
-     if(evtObj.type === 'click' && evtObj.target.classList.contains('btn-danger')){
-            var labelEl = evtObj.target.parentElement;
-
-                   onToggleItem(labelEl);
-        }
-}
-
-
-var a=0;
-
-
-function delegateEvent10(evtObj) {
-
-    if (evtObj.type === 'click' && evtObj.target.classList.contains('btn-success') && control==1) {
-        var nameText2 = document.getElementById('msgspace');
-        Mess = nameText2.value;
-
-        changeDescription(MessageList[a], Mess);
-        updateItem(labelEl1, MessageList[a]);
-        store(MessageList);
-        control = 0;
-    }
-}
-
-
-function changeMessage(labelEl){
-    var id = labelEl.attributes['data-task-id'].value;
-
-
-    for(var i = 0; i < MessageList.length; i++) {
-        if (MessageList[i].id != id)
-            continue;
-
-         a=i;
-         Mess = MessageList[i].description2;
-        document.getElementById('msgspace').value = Mess;
-        control=1;
-        var appContainer = document.getElementsByClassName('btn-success')[0];
-        appContainer.addEventListener('click', delegateEvent10);
-             return;
-        }
-
-}
-
-function changeDescription(message,newMess){
-    message.description2 =newMess ;
-    var nameText2 = document.getElementById('msgspace');
-    nameText2.value = '';
-}
-
-
-function onToggleItem(labelEl) {
-    var id = labelEl.attributes['data-task-id'].value;
-    for(var i = 0; i < MessageList.length; i++) {
-        if(MessageList[i].id != id)
-            continue;
-        MessageList.splice(i,1);
-        store(MessageList);
-        location.reload()
-
-        return;
-    }
-
-}
-
-function sendMes(){
-    var appContainer = document.getElementsByClassName('btn-success')[0];
-    appContainer.addEventListener('click', delegateEvent4);
-
-}
-function delegateEvent4(evtObj) {
-
-    if(evtObj.type === 'click' && evtObj.target.classList.contains('btn-success') && control!=1){
-    send();
-    }
-
-}
-function send() {
-
-        var nameText2 = document.getElementById('msgspace');
-        var newMes = theMessage(user,nameText2.value);
-        if(nameText2.value!='' && user!=""){
-            addMsg(newMes);
-
-        }
-        if(user=="")  {
-        alert("Введите имя пользователя") ;
-        }
-        nameText2.value = '';
-
-
-
-
-}
-
-
-
-function editName(){
-
-    document.getElementById('edit').onclick = function () {
-        var newName= prompt("Введите ваше новое имя");
-        addName(newName);
-    }
-
-}
-
-
-
-
-function addMsg(message) {
-
-    document.addEventListener("DOMContentLoaded", function addMsg() {}, false);
-
-    var item = createItem(message);
-    var items = document.getElementsByClassName('layer')[0];
-
-    MessageList.push(message);
-    items.appendChild(item);
-    store(MessageList);
-}
-
-function createItem(message){
-    var temp = document.createElement('div');
-    var htmlAsText = '<div data-task-id="идентификатор">описание'+ '<button class="btn-danger edit-btn" >'+'<button class="btn-warning edit-btn" ></div>';
-    temp.innerHTML = htmlAsText;
-    updateItem(temp.firstChild, message);
-
-    return temp.firstChild;
-}
-
-function updateItem(divItem, message){
-
-    divItem.setAttribute('data-task-id', message.id);
-    divItem.firstChild.textContent = message.description+": "+message.description2;
-
-}
-function store(listToSave) {
-
-
-    if(typeof(Storage) == "undefined") {
-        alert('localStorage is not accessible');
-        return;
-    }
-
-    localStorage.setItem("MessageList", JSON.stringify(listToSave));
-}
-function storeName(nameToSave) {
-
-
-    if(typeof(Storage) == "undefined") {
-        alert('localStorage is not accessible');
-        return;
-    }
-
-    localStorage.setItem("user", JSON.stringify(nameToSave));
-}
-
-function restoreName() {
-    if(typeof(Storage) == "undefined") {
-        alert('localStorage is not accessible');
-        return;
-    }
-
-    var item = localStorage.getItem("user");
-
-    return item && JSON.parse(item);
-}
-function restore() {
-    if(typeof(Storage) == "undefined") {
-        alert('localStorage is not accessible');
-        return;
-    }
-
-    var item = localStorage.getItem("MessageList");
-
-    return item && JSON.parse(item);
-}
-
-
-function run() {
-    var element = document.getElementsByClassName('MyEl')[0];
-
-    element.addEventListener('click', delegateEvent);
-    user=restoreName();
-    addName(user);
-    var allMessages = restore() || [ theMessage('Вика','Привет' )];
-    createAllMessages(allMessages);
-
-
-}
-function createAllMessages(allMessages) {
-    for(var i = 0; i < allMessages.length; i++)
-        addMsg(allMessages[i]);
-}
-
-
-
-function delegateEvent(evtObj) {
-
-    var nameText = document.getElementById('nameText');
-
-    text=nameText.value;
-    if(text!=''){
-
-        addName(text);
-
-    }
-    nameText.value = '';
-
-}
-
-
-function addName(value) {
-    if(!value){
-        return;
-    }
-
-
-
-    document.addEventListener( "DOMContentLoaded", function addName() {}, false );
-    user=value;
-    document.getElementById('myName').innerHTML=value;
-    storeName(user);
-
-}
 
 
 function func(){
@@ -273,4 +38,301 @@ function func(){
 
 
 }
+function run() {
+    var element = document.getElementsByClassName('MyEl')[0];
 
+    element.addEventListener('click', delegateEvent);
+
+}
+function whatToDo(){
+    var appContainer = document.getElementsByClassName('Messages')[0];
+    appContainer.addEventListener('click', delegateEvent5);
+}
+var editlabel;
+function delegateEvent5(evtObj) {
+
+    if(evtObj.type === 'click' && evtObj.target.classList.contains('btn-warning')){
+        changeMessage(evtObj.target.parentElement);
+    }
+    if(evtObj.type === 'click' && evtObj.target.classList.contains('btn-danger')){
+        onToggleItem(evtObj.target.parentElement);
+    }
+}
+
+function onToggleItem(divItem) {
+    var id = divItem.attributes['data-task-id'].value;
+    var messageList = appState.messageList;
+    for(var i = 0; i < messageList.length; i++) {
+        if(messageList[i].id != id)
+            continue;
+
+        toggle(messageList[i], function() {
+            updateItem2(divItem, messageList[i]);
+            output(messageList);
+        });
+
+        return;
+    }
+
+}
+var a=0;
+function changeMessage(divItem){
+    var id = divItem.attributes['data-task-id'].value;
+    var messageList = appState.messageList;
+    editlabel=divItem;
+    for(var i = 0; i < messageList.length; i++) {
+        if(messageList[i].id != id)
+            continue;
+
+        a=i;
+        var Mess = messageList[i].message;
+        document.getElementById('msgspace').value = Mess;
+        control=1;
+        var appContainer = document.getElementsByClassName('btn-success')[0];
+        appContainer.addEventListener('click', delegateEvent10);
+        return;
+    }
+
+}
+function delegateEvent10(evtObj) {
+
+    if (evtObj.type === 'click' && evtObj.target.classList.contains('btn-success') && control==1) {
+        var nameText2 = document.getElementById('msgspace');
+        var Mess = nameText2.value;
+        var messageList = appState.messageList;
+        changeDescription(messageList[a], Mess,function() {
+            updateItem2(editlabel, messageList[a]);
+            output(messageList);
+        });
+
+
+
+        control = 0;
+    }
+}
+
+function changeDescription(message,newMess,continueWith) {
+    message.message =newMess ;
+    var nameText2 = document.getElementById('msgspace');
+    nameText2.value = '';
+
+    put(appState.mainUrl + '?id=' + message.id, JSON.stringify(message), function() {
+        continueWith();
+    });
+
+}
+
+
+function toggle(message, continueWith) {
+    message.message="";
+    put(appState.mainUrl + '?id=' + message.id, JSON.stringify(message), function() {
+        continueWith();
+    });
+}
+
+
+function delegateEvent(evtObj) {
+
+    var nameText = document.getElementById('nameText');
+
+    var text=nameText.value;
+    if(text!=''){
+
+       addName(text);
+
+    }
+    nameText.value = '';
+
+}
+function addName(value) {
+    if(!value){
+        return;
+    }
+
+
+
+    document.addEventListener( "DOMContentLoaded", function addName() {}, false );
+    user=value;
+    document.getElementById('myName').innerHTML=value;
+}
+
+
+function sendMes(){
+    var appContainer = document.getElementsByClassName('btn-success')[0];
+    appContainer.addEventListener('click', delegateEvent4);
+
+    restore();
+
+}
+function createAllMessages(allMessages) {
+    for(var i = 0; i < allMessages.length; i++) {
+        addMsgInternal(allMessages[i]);
+        if(i==allMessages.length-1)
+        {
+            restoreName(allMessages[i]);
+        }
+    }
+}
+function delegateEvent4(evtObj) {
+
+    if(evtObj.type === 'click' && evtObj.target.classList.contains('btn-success') && control!=1){
+        send();
+    }
+
+}
+function send() {
+
+    var nameText2 = document.getElementById('msgspace');
+    var newMes = theMessage(user,nameText2.value);
+    if(nameText2.value == '')
+        return;
+
+
+        addMsg(newMes,function() {
+            output(appState);
+        });
+
+    nameText2.value = '';
+
+}
+
+function addMsg(message, continueWith) {
+    post(appState.mainUrl, JSON.stringify(message), function(){
+        restore();
+    });
+}
+
+function editName (){
+
+    document.getElementById('edit').onclick = function () {
+        var newName= prompt("Введите ваше новое имя");
+        addName(newName);
+    }
+
+}
+
+
+function addMsgInternal(message) {
+
+
+    var item = createItem(message);
+    var items = document.getElementsByClassName('layer')[0];
+    var messageList = appState.messageList;
+    messageList.push(message);
+    items.appendChild(item);
+
+}
+function updateItem2 (divItem, message){
+
+    divItem.setAttribute('data-task-id', message.id);
+    divItem.firstChild.textContent = message.user+": "+message.message;
+
+}
+
+function createItem(message){
+    var temp = document.createElement('div');
+    var htmlAsText = '<div data-task-id="идентификатор">описание'+ '<button class="btn-danger edit-btn" >'+'<button class="btn-warning edit-btn" ></div>';
+    temp.innerHTML = htmlAsText;
+    updateItem2(temp.firstChild, message);
+
+    return temp.firstChild;
+}
+function restoreName (message){
+   addName(message.user);
+}
+
+function restore(continueWith) {
+    var url = appState.mainUrl + '?token=' + appState.token;
+    get(url, function(responseText) {
+        console.assert(responseText != null);
+
+        var response = JSON.parse(responseText);
+
+        appState.token = response.token;
+        createAllMessages(response.messages);
+        output(appState);
+
+
+
+        continueWith && continueWith();
+    });
+
+}
+
+function output(value){
+    var output = document.getElementById('output');
+
+    output.innerText = JSON.stringify(value, null, 2);
+}
+
+function defaultErrorHandler(message) {
+    console.error(message);
+    output(message);
+}
+function get(url, continueWith, continueWithError) {
+    ajax('GET', url, null, continueWith, continueWithError);
+}
+
+function post(url, data, continueWith, continueWithError) {
+    ajax('POST', url, data, continueWith, continueWithError);
+}
+
+function put(url, data, continueWith, continueWithError) {
+    ajax('PUT', url, data, continueWith, continueWithError);
+}
+function isError(text) {
+    if(text == "")
+        return false;
+
+    try {
+        var obj = JSON.parse(text);
+    } catch(ex) {
+        return true;
+    }
+
+    return !!obj.error;
+}
+
+function ajax(method, url, data, continueWith, continueWithError) {
+    var xhr = new XMLHttpRequest();
+
+    continueWithError = continueWithError || defaultErrorHandler;
+    xhr.open(method || 'GET', url, true);
+
+    xhr.onload = function () {
+        if (xhr.readyState !== 4)
+            return;
+
+        if(xhr.status != 200) {
+            continueWithError('Error on the server side, response ' + xhr.status);
+            return;
+        }
+
+        if(isError(xhr.responseText)) {
+            continueWithError('Error on the server side, response ' + xhr.responseText);
+            return;
+        }
+
+        continueWith(xhr.responseText);
+    };
+
+    xhr.ontimeout = function () {
+        continueWithError('Server timed out !');
+    }
+
+    xhr.onerror = function (e) {
+        var errMsg = 'Server connection error !\n'+
+            '\n' +
+            'Check if \n'+
+            '- server is active\n'+
+            '- server sends header "Access-Control-Allow-Origin:*"';
+
+        continueWithError(errMsg);
+    };
+
+    xhr.send(data);
+}
+
+window.onerror = function(err) {
+    output(err.toString());
+}
