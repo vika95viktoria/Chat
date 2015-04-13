b5a77ec10fb66b2236b695215e3afc32e17de63f
unknown <vika95vika@gmail.com>
2015-04-13 21:13:16 +0300

    MessageExchange

--- MessageExchange.java
+++ MessageExchange.java
@@ -5,13 +5,13 @@ import org.json.simple.parser.ParseException;
 import java.io.*;
 import java.util.List;
 
-
 public class MessageExchange {
 
     private JSONParser jsonParser = new JSONParser();
 
     public String getToken(int index) {
         Integer number = index * 8 + 11;
+
         return "TN" + number + "EN";
     }
 
@@ -19,41 +19,44 @@ public class MessageExchange {
         return (Integer.valueOf(token.substring(2, token.length() - 2)) - 11) / 8;
     }
 
-    public String getServerResponse(List<JSONObject> messages) {
-        JSONObject obj = new JSONObject();
-        obj.put("messages",messages);
-        obj.put("token", getToken(messages.size()));
-        return obj.toJSONString();
-    }
-
-    public String getClientSendMessageRequest(String user, String message, int id) {
+    public String getServerResponse(List<Message> messages, int index) {
+        List<Message> chunk = messages.subList(index, messages.size());
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("message", message);
-        jsonObject.put("user", user);
-        jsonObject.put("id", id);
+
+        jsonObject.put("messages", chunk);
+        jsonObject.put("token", getToken(messages.size()));
+
         return jsonObject.toJSONString();
     }
 
-    public JSONObject getClientMessage(InputStream inputStream) throws ParseException {
-        return  getJSONObject(inputStreamToString(inputStream));
+    public Message getClientMessage(InputStream inputStream) throws Exception {
+        JSONObject json = getJSONObject(inputStreamToString(inputStream));
+
+        return new Message((String)json.get("id"), (String)json.get("message"), (String)json.get("user") );
+    }
+
+    public String getErrorMessage(String text) {
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("error", text);
+
+        return jsonObject.toJSONString();
     }
 
     public JSONObject getJSONObject(String json) throws ParseException {
         return (JSONObject) jsonParser.parse(json.trim());
     }
 
-    public String inputStreamToString(InputStream in) {
+    public String inputStreamToString(InputStream in) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         byte[] buffer = new byte[1024];
         int length = 0;
-        try {
-            while ((length = in.read(buffer)) != -1) {
-                baos.write(buffer, 0, length);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+
+        while ((length = in.read(buffer)) != -1) {
+            baos.write(buffer, 0, length);
         }
 
+        System.out.println("Input stream " + new String(baos.toByteArray()));
         return new String(baos.toByteArray());
     }
 }
